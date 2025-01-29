2331d949b9e0a44e95773a38053a746acf4cfe16
Eveloth <vadim.stepanov.web@gmail.com>
2025-01-30 01:09:33 +0300

    nvim: update lualine

--- eveloth/dot_config/nvim/lua/eveloth/lualine.lua
+++ eveloth/dot_config/nvim/lua/eveloth/lualine.lua
@@ -1,23 +1,29 @@
 local M = {
-  "nvim-lualine/lualine.nvim"
+	"nvim-lualine/lualine.nvim",
 }
 
 function M.config()
-  require("lualine").setup {
-    options = {
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      ignore_focus = { "NvimTree" },
-    },
-    sections = {
-      lualine_a = { "filename"},
-      lualine_b = { "branch" },
-      lualine_c = { "diagnostics" },
-      lualine_y = { "progress" },
-      lualine_z = {},
-    },
-    extensions = { "quickfix", "man", "fugitive" },
-  }
+	local filename = {
+		"filename",
+		separator = { left = "", right = "" },
+	}
+
+	require("lualine").setup({
+		options = {
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			ignore_focus = { "NvimTree" },
+		},
+		sections = {
+			lualine_a = { filename },
+			lualine_b = { "branch" },
+			lualine_c = { "diagnostics" },
+			lualine_x = { "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = {},
+		},
+		extensions = { "quickfix", "man", "fugitive" },
+	})
 end
 
 return M
