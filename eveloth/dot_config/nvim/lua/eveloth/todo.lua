1ce6fc9d13f33e2ed42166f9a393f14032791e58
Eveloth <vadim.stepanov.web@gmail.com>
2025-02-03 23:09:49 +0300

    nvim: add todo expolorer keybinding

--- eveloth/dot_config/nvim/lua/eveloth/todo.lua
+++ eveloth/dot_config/nvim/lua/eveloth/todo.lua
@@ -1,12 +1,18 @@
 local M = {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
-  event ={ "BufReadPost", "BufWritePost", "BufNewFile" },
-  opts = {}
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	opts = {},
 }
 
 M.config = function()
-  require("todo-comments").setup()
+  local todo = require("todo-comments")
+  todo.setup()
+
+  local wk = require("which-key")
+  wk.add({
+    {"<leader>fw", "<cmd>TodoTelescope<cr>", decs = "Find TODOs"}
+  })
 end
 
 return M
