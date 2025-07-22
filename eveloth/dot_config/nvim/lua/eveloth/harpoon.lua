0594a7bb52d7b0b3fb6d3780701fc359d6cd3f97
Eveloth <vadim.stepanov.web@gmail.com>
2025-07-22 18:05:47 +0300

    nvim: add harpoon2

--- eveloth/dot_config/nvim/lua/eveloth/harpoon.lua
+++ eveloth/dot_config/nvim/lua/eveloth/harpoon.lua
@@ -1,30 +1,44 @@
 local M = {
 	"ThePrimeagen/harpoon",
-	event = "VeryLazy",
+	branch = "harpoon2",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
 	},
 }
 
-function M.config()
-	local mark = require("harpoon.mark")
-	local ui = require("harpoon.ui")
+M.config = function()
+	local harpoon = require("harpoon")
 	local wk = require("which-key")
+	harpoon:setup()
+
+	local toggle_menu = function()
+		harpoon.ui:toggle_quick_menu(harpoon:list())
+	end
+
+	local add = function()
+		harpoon:list():add()
+	end
 
 	local function nav(x)
 		return function()
-			ui.nav_file(x)
+			harpoon:list():select(x)
 		end
 	end
 
-	wk.add({ "<leader>h", group = "Harpoon" }, { "<leader>h", ui.toggle_quick_menu }, { "<leader>a", mark.add_file })
+	wk.add({
+		{ "<leader>h", group = "Harpoon" },
+		{ "<leader>hh", toggle_menu, desc = "Toggle menu" },
+		{ "<leader>ha", add, desc = "Add" },
+	})
 
 	wk.add({
-		hidden = true,
-		{ "<leader>1", nav(1) },
-		{ "<leader>1", nav(2) },
-		{ "<leader>1", nav(3) },
-		{ "<leader>1", nav(4) },
+		{
+			hidden = true,
+			{ "<leader>1", nav(1) },
+			{ "<leader>2", nav(2) },
+			{ "<leader>3", nav(3) },
+			{ "<leader>4", nav(4) },
+		},
 	})
 end
 
--- eveloth/dot_config/nvim/lua/eveloth/oil.lua
+++ eveloth/dot_config/nvim/lua/eveloth/oil.lua
@@ -1,17 +1,21 @@
 local M = {
-  "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
 }
 
 function M.config()
-  require("oil").setup()
+	require("oil").setup()
+	local wk = require("which-key")
+	wk.add({
+		"<leader>dr",
+		function()
+			vim.cmd("Oil")
+		end,
+	})
 end
 
 return M
