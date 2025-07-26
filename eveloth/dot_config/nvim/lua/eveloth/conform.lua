d2edea56a329d19e1b8269318168ba7a7b5653dd
Eveloth <vadim.stepanov.web@gmail.com>
2025-07-26 18:59:50 +0300

    nvim: update conform

--- eveloth/dot_config/nvim/lua/eveloth/conform.lua
+++ eveloth/dot_config/nvim/lua/eveloth/conform.lua
@@ -4,10 +4,15 @@ local M = {
 
 M.config = function()
 	require("conform").setup({
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
 		formatters_by_ft = {
 			cs = { "csharpier" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			lua = { "stylua" },
+			haskell = { "fourmolu" },
 		},
 		formatters = {
 			csharpier = {
