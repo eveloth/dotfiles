a47bb032baa71fbb1f389ef29389bf252a72c6db
Eveloth <vadim.stepanov.web@gmail.com>
2025-08-01 08:09:57 +0300

    zsh: add yc cli

---
+++ eveloth/dot_config/zsh/yc.sh
@@ -0,0 +1,6 @@
+# The next line updates PATH for CLI.
+if [ -f '/home/eveloth/yandex-cloud/path.bash.inc' ]; then source '/home/eveloth/yandex-cloud/path.bash.inc'; fi
+
+# The next line enables shell command completion for yc.
+if [ -f '/home/eveloth/yandex-cloud/completion.zsh.inc' ]; then source '/home/eveloth/yandex-cloud/completion.zsh.inc'; fi
+
