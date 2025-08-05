7955bda5788c671d039df9e70af53a1870d77bb4
Vadim Komissarov <vadim.stepanov.web@gmail.com>
2025-08-05 16:12:24 +0300

    nvim: polish cmp config

--- eveloth/dot_config/nvim/lua/eveloth/blink-cmp.lua
+++ eveloth/dot_config/nvim/lua/eveloth/blink-cmp.lua
@@ -4,51 +4,58 @@ local M = {
 		"rafamadriz/friendly-snippets",
 		"folke/lazydev.nvim",
 	},
-	-- use a release tag to download pre-built binaries
 	version = "1.*",
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		keymap = { preset = "default" },
+	opts = {},
+	opts_extend = { "sources.default" },
+}
 
+M.config = function()
+	require("blink-cmp").setup({
+		enabled = function()
+			local ft = vim.bo[0].filetype
+			return not (ft == "snacks_picker_input")
+		end,
+		keymap = {
+			preset = "default",
+		},
+		cmdline = {
+			enabled = true,
+		},
 		completion = {
 			menu = {
-				auto_show = false,
+				auto_show = true,
 				border = "rounded",
+			},
+			documentation = {
+				auto_show = false,
+				window = {
+					border = "rounded",
+				},
 			},
 			ghost_text = {
 				show_with_selection = true,
 				show_without_menu = true,
 				show_without_selection = true,
-				show_with_menu = true,
+				show_with_menu = false,
 				enabled = true,
 			},
-			documentation = { auto_show = false },
 		},
-
 		sources = {
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 			providers = {
+				lsp = {
+					-- make LSP top priority
+					score_offset = 90,
+				},
 				lazydev = {
 					name = "LazyDev",
 					module = "lazydev.integrations.blink",
-					-- make lazydev completions top priority (see `:h blink.cmp`)
+					-- ..but lazydev even higher
 					score_offset = 100,
 				},
 			},
 		},
-
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
-	opts_extend = { "sources.default" },
-}
-
-M.config = function()
-	require("blink-cmp").setup({})
+	})
 end
 
 return M
