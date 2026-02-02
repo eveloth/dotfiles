569cebbae85156341cec334f9f56021fd49006e4
Vadim Komissarov <komissarov@errors.is>
2026-02-02 22:24:16 +0300

    zsh: fix mise

--- eveloth/dot_config/zsh/mise.sh
+++ eveloth/dot_config/zsh/mise.sh
@@ -1,1 +1,4 @@
+# It will append to the path if you don't do this; I think custom user paths should be evaluated first anyways
+export MISE_ACTIVATE_AGGRESSIVE=1
+
 [[ $(command -v mise) ]] && eval "$(mise activate zsh)"
